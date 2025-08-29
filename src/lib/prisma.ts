b79efd3cdf196a24631a6ff9849e547c5741e67a
Kae Esport <thescriptkiddies22@gmail.com>
2025-08-29 01:28:12 +0000

    oke don't run with production, just with development cuz we still develo

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,11 +1,17 @@
 import { PrismaClient } from '@prisma/client';
 
+const prismaClientSingleton = () => {
+  return new PrismaClient();
+};
+
 declare global {
-  var prisma: PrismaClient | undefined;
+  var prisma: undefined | ReturnType<typeof prismaClientSingleton>;
 }
 
-export const db = globalThis.prisma || new PrismaClient();
+const db = globalThis.prisma ?? prismaClientSingleton();
 
 if (process.env.NODE_ENV !== 'production') {
   globalThis.prisma = db;
 }
+
+export { db };
